62798a63e391cd0cf1d7b015d4c5bc0dd8c33cc7
Alper Demir <105297896+Hololopoto@users.noreply.github.com>
2024-03-21 16:30:47 +0300

    Add tic-tac-toe game functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,83 @@
 import "./App.css";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+
+  const handleClick = (index) => {
+    if (board[index] || calculateWinner(board)) {
+      return;
+    }
+
+    const newBoard = [...board];
+    newBoard[index] = isXNext ? "X" : "O";
+    setBoard(newBoard);
+    setIsXNext(!isXNext);
+  };
+
+  const renderSquare = (index) => {
+    return (
+      <button
+        className="square items-center border-orange-800 border-4 justify-center w-10 h-10 bg-slate-500"
+        onClick={() => handleClick(index)}>
+        {board[index]}
+      </button>
+    );
+  };
+
+  const winner = calculateWinner(board);
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${isXNext ? "X" : "O"}`;
+
+  return (
+    <div className="App">
+      <div className="board">
+        <div className="status">{status}</div>
+        <div className="">
+          <div className="row m-auto flex flex-row ">
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+          </div>
+          <div className="row   ">
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+          </div>
+          <div className="row ">
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
+
+function calculateWinner(board) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a];
+    }
+  }
+
+  return null;
+}
